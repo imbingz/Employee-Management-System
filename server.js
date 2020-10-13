98b116c503aa402e41e26f8de4f3c9990141e3f4
Bing Z <bing@bings-mbp.lan>
2020-10-13 16:42:08 -0600

    added view emp by dept, mangers  funcs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // IMPORT PACKAGES AND MODULES =============================================================================================
 const startDisplay = require('./lib/starter');
 const connection = require('./lib/connection');
-const questions = require('./lib/questions');
+
 
 const { prompt } = require('inquirer');
 const cTable = require('console.table');
@@ -22,7 +22,14 @@ startDisplay();
 // }
 
 function start() {
-  prompt(questions)
+  prompt(
+    {
+      name: "task",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View all employees", "View all employees by Department", "View all employees by Managers", "Add employees", "Remove employees", "Update employees", "Update employee Manager"]
+    }
+  )
     .then(answer => { 
       
       switch (answer.task) {
@@ -79,7 +86,25 @@ function readAllEmployees() {
 function readEmpByDept() {
   //SELECT all employeeID, name, title, salary, department ORDERED by department 
   connection.query(
-    "SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.name AS department FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d on r.department_id = d.id",
+    "SELECT d.id, d.name AS department, r.title, e.first_name, e.last_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM department d LEFT JOIN role r on r.department_id = d.id LEFT JOIN employee e ON e.role_id = r.id LEFT JOIN employee m ON m.id = e.manager_id",
+
+    (err, res) => {
+      if (err) throw err;
+
+      //Display data in table format 
+      console.table("\n", res);
+
+      //Start prompting the questions again
+      start();
+    }
+  );
+};
+
+//Function to handle "View all employees by managers"
+function readEmpByMngr() {
+  //SELECT all managerID, name, title, salary, department ORDERED BY manager_id
+  connection.query(
+    "SELECT m.manager_id, m.first_name, m.last_name, r.title, r.salary, d.name AS department FROM employee m LEFT JOIN employee e ON m.id = e.manager_id LEFT JOIN role r on m.role_id = r.id LEFT JOIN department d on r.department_id = d.id",
 
     (err, res) => {
       if (err) throw err;
@@ -94,13 +119,10 @@ function readEmpByDept() {
 };
 
 
-
-
-
-
-
-
-
+//Function to handle "Add employees"
+function insertEmployees() {
+  prompt().then(answer => {})
+}
 
 
 
