e7eaded7f9b3d4a9f35ad7749683b48f18772789
Bing Z <bing@bings-mbp.lan>
2020-10-13 12:40:32 -0600

    imported modules n initial connection

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-const connection = require('./connection');
+// IMPORT PACKAGES AND MODULES ================================================================================
 
+const connection = require('./lib/connection');
+const startMessage = require('./lib/starter');
+const { prompt } = require('inquirer');
+
+// DISPLAY EMPLOYEE TRACKER WHEN STARTING THE APP ================================================================================
+
+startMessage();
+
+
+// PROMPTING QUESTIONS ================================================================================
